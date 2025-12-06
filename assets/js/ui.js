ca82224de3013b5b4e099d3381bb30f89899ce82
Giuseppe Tavella <giuseppetavella8@gmail.com>
2025-12-06 20:39:47 +0100

    start UI-data work

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -1,12 +1,73 @@
+/**
+ * 
+ * 
+ * SCHEMA CONCRETO
 
+ * {
+ *    nomeRicetta: str
+ *    ingredienti: [
+ *       {
+ *          nome: str
+ *          quantita: float
+ *          _ui: {
+                nome: htmlEl
+                quantita: htmlEl
+ *          }
+ *       },
+ *       ...
+ *    ]  
+ * }
+ * 
+ * Nella proprietà _ui_input ci sono i campi UI da dove
+ * i valori di quelle stesse proprietà verranno estratti.
+ */
+const ricettaUtente = {
+  nome: null,
+  ingredienti: [],
+};
 
-function avviaEsempioUI() {
-    const elementoTestoEsempio = document.getElementById("esempio-testo")
-    // console.log(elementoTestoEsempio)
-    const testo = generaStoriaEsempio()
-    typeWrite(testo, elementoTestoEsempio)
+// const ricettaUI = {
+//   tableRicetta: null,
+//   tableRicettaRows: null,
+// };
 
-    // elementoTestoEsempio.textContent = testo
+// appena la pagina è caricata, aggiungi una riga
+
+function aggiornaIngredienti(ingredienti, ricetta) {}
+
+// AGGIUNGI CAMPO INGREDIENTE UI
+
+function aggiungiCampoIngredienteUI() {
+  const htmlTable = document.getElementById("table-ricetta");
+  const htmlBody = htmlTable.querySelectorAll("tbody")[0];
+  //   console.log(htmlTable)
+  //   console.log(htmlBody)
 }
 
+// AGGIUNGI INGREDIENTE
 
+function aggiungiIngrediente() {}
+
+function aggiungiIngredienteInUI() {}
+
+function aggiungiIngredienteInDato() {}
+
+// RIMUOVI INGREDIENTE
+
+function rimuoviIngrediente() {}
+
+// TROVA INGREDIENTE
+
+function trovaIngrediente() {}
+
+/**
+ * Avvia l'esempio/la narrazione della ricetta nell'UI.
+ */
+function avviaEsempioUI() {
+  const elementoTestoEsempio = document.getElementById("esempio-testo");
+  // console.log(elementoTestoEsempio)
+  const testo = generaStoriaEsempio();
+  typeWrite(testo, elementoTestoEsempio);
+
+  // elementoTestoEsempio.textContent = testo
+}
