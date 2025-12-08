43ea44495cfd4f1a21534d93b794e7eb5929d514
Giuseppe Tavella <giuseppetavella8@gmail.com>
2025-12-09 00:21:45 +0100

    calcFromTot done, clean OOP

--- assets/js/RecipeCalculator.js
+++ assets/js/RecipeCalculator.js
@@ -61,216 +61,6 @@
  * ]
  *
  */
-// class RecipeCalculator {
-//   constructor(recipeName, recipeAsList) {
-//     this.recipeName = recipeName;
-//     this.recipe = new Recipe(recipeAsList);
-//     this.recipeInstance = new RecipeInstance(recipeAsList);
-//     // console.log(this);
-//     this.calcProportions();
-//   }
-
-//   //   CORE
-
-//   /**
-//    *
-//    */
-//   calcProportions() {
-//     // how many ingredients are there
-//     const nIngredients = this.recipe.ingredients.length;
-//     for (let i = 0; i < nIngredients; i++) {
-//       const ingredient = this.recipe.ingredients[i];
-//       const ingredientInstance = this.recipeInstance.ingredients[i];
-//       // la proporzione del singolo ingrediente è data dalla sua quantita
-//       // rispetto alla quantità totale
-//       const proportion = ingredientInstance.quantity / this.recipeInstance.totIngredients;
-
-//       ingredient._setProportion(proportion, ingredientInstance);
-//       ingredient._setPercentage(proportion, ingredientInstance);
-//     }
-//     return this;
-//   }
-
-//   /**
-//    *
-//    */
-//   calcFromIngredient({ name: ingredientKnown, quantity: quantityKnown }) {
-//     const newRecipeAsList = [];
-//     //   trova proporzione dell'ingrediente noto, dalle proporzioni personalizzate
-//     const proportionKnown = this._findProportionOfIngredient(ingredientKnown);
-//     // visto che il totale delle proporzioni sarà sempre 1, allora
-//     // la proporzione rimanente si ottiene sottraendo  1 - proporzioneNota
-//     const proportionKnownRemaining = 1 - proportionKnown;
-//     // la quantita rimanente del totale
-//     // si basa sulla seguente proporzione
-//     // quantitaNota : quantitaRimanente = proporzioneNota : proporzioneNotaRimanente
-//     // esempio:
-//     // 10 g di sale stanno alla quantità di impasto che rimane, come la proporzione del sale (nel totale impasto)
-//     // sta al totale delle proporzioni degli altri ingredienti
-//     const quantityRemaining = (quantityKnown * proportionKnownRemaining) / proportionKnown;
-//     // alla fine, dalla quantita nota di un ingrediente, e dalle proporzioni della ricetta,
-//     // si ricava la quantità totale di questo (nuovo?) impasto, non di quello dato
-//     // prima dall'utente come input
-//     const totNewQuantity = quantityKnown + quantityRemaining;
-
-//     const nIngredients = this.recipe.ingredients.length;
-//     for (let i = 0; i < nIngredients; i++) {
-//       // always refer to the real ingredient (so the proportion)
-//       const ingredient = this.recipe.ingredients[i];
-//       // moltiplicando la quantità totale con la proporzione del singolo ingrediente,
-//       // ricavo finalmente la quantità di ogni altro ingrediente, oltre all'ingrediente dato
-//       const quantityNewIngredient = totNewQuantity * ingredient.proportion;
-//       const quantityRoundedNewIngredient = ingredient._roundQuantity(quantityNewIngredient);
-//       // arrotondamenti e non
-
-//       // console.log(ingredient)
-
-//       const newIngredient = {
-//         name: ingredient.name,
-//         proportion: ingredient.proportion,
-//         percentage: ingredient.percentage,
-//         percentageRounded: ingredient.percentageRounded,
-//         quantity: quantityNewIngredient,
-//         quantityRounded: quantityRoundedNewIngredient,
-//       };
-
-//       console.log(newIngredient)
-//       newRecipeAsList.push(newIngredient);
-//     }
-//     return new RecipeInstance(newRecipeAsList);
-//   }
-
-//   calcFromTot() {}
-
-//   // HELPERS
-
-//   _findProportionOfIngredient(ingredientName) {
-//     for (let i = 0; i < this.recipe.ingredients.length; i++) {
-//       const ingredient = this.recipe.ingredients[i];
-//       // se trovo l'ingrediente che mi interessa
-//       if (ingredient.name === ingredientName) {
-//         return ingredient.proportion;
-//       }
-//     }
-//     throw Error("Non è stato trovato nessuna proporzione per l'ingrediente dato.");
-//   }
-
-//   _roundNumber(x, nDigits) {
-//     return parseFloat(x.toFixed(nDigits));
-//   }
-
-//   _roundProportion(x) {
-//     return this._roundNumber(x, 4);
-//   }
-
-//   _roundPercentage(x) {
-//     return this._roundNumber(x, 2);
-//   }
-
-//   _roundQuantity(x) {
-//     return this._roundNumber(x, 2);
-//   }
-// }
-
-// class Ingredient {
-//   constructor(ingredient) {
-//     this.name = ingredient.name;
-//     this.proportion = ingredient.proportion;
-//     this.proportionRounded = ingredient.proportion;
-//     this.percentage = null;
-//     this.percentageRounded = null;
-//   }
-
-//   /**
-//    * Set the proportion for the ingredient as well as
-//    * for the ingredient instance, so they are in sync.
-//    */
-//   _setProportion(proportion, ingredientInstance) {
-//     const proportionRounded = this._roundProportion(proportion);
-//     this.proportion = proportion;
-//     this.proportionRounded = proportionRounded;
-//     ingredientInstance.proportion = proportion;
-//     ingredientInstance.proportionRounded = proportionRounded;
-//   }
-
-//   /**
-//    * Set the percentage for the ingredient as well as
-//    * for the ingredient instance, so they are in sync.
-//    */
-//   _setPercentage(proportion, ingredientInstance) {
-//     const percentage = proportion * 100;
-//     const percentageRounded = this._roundPercentage(percentage);
-//     this.percentage = percentage;
-//     this.percentageRounded = percentageRounded;
-//     ingredientInstance.percentage = percentage;
-//     ingredientInstance.percentageRounded = percentageRounded;
-//   }
-
-//   // HELPERS
-
-//   _roundNumber(x, nDigits) {
-//     return parseFloat(x.toFixed(nDigits));
-//   }
-
-//   _roundProportion(x) {
-//     return this._roundNumber(x, 4);
-//   }
-
-//   _roundPercentage(x) {
-//     return this._roundNumber(x, 2);
-//   }
-
-//   _roundQuantity(x) {
-//     return this._roundNumber(x, 2);
-//   }
-
-//   genRandNum(limit = 10000000) {
-//     return Math.floor(Math.random() * limit);
-//   }
-// }
-
-// class IngredientInstance extends Ingredient {
-//   constructor(ingredientInstance) {
-//     super(ingredientInstance);
-//     this.quantity = ingredientInstance.quantity;
-//     this.quantityRounded = ingredientInstance.quantityRounded !== null ? ingredientInstance.quantityRounded : ingredientInstance.quantity;
-//   }
-// }
-
-// class Recipe {
-//   constructor(recipeAsList) {
-//     this.ingredients = [];
-//     // console.log(recipeAsList);
-//     for (let i = 0; i < recipeAsList.length; i++) {
-//       const ingredientObj = recipeAsList[i];
-//       const ingredientInstance = new Ingredient(ingredientObj);
-//       this.ingredients.push(ingredientInstance);
-//     }
-//   }
-// }
-
-// class RecipeInstance extends Recipe {
-//   constructor(recipeAsList) {
-//     super(recipeAsList);
-//     this.ingredients = [];
-//     this.totIngredients = 0;
-//     for (let i = 0; i < recipeAsList.length; i++) {
-//       const ingredientObj = recipeAsList[i];
-//       const ingredientInstance = new IngredientInstance(ingredientObj);
-//       this.ingredients.push(ingredientInstance);
-//     }
-//     this.calcTotIngredients();
-//   }
-
-//   calcTotIngredients() {
-//     this.totIngredients = 0;
-//     for (let i = 0; i < this.ingredients.length; i++) {
-//       const ingredientInstance = this.ingredients[i];
-//       this.totIngredients += ingredientInstance.quantity;
-//     }
-//     return this;
-//   }
-// }
 
 class Recipe {
   constructor(recipeName, ingredientsList) {
@@ -346,6 +136,20 @@ class Recipe {
     return newRecipeInstance;
   }
 
+  calcFromTot(totIngredientsKnown) {
+    const newRecipeInstance = new RecipeInstance([], this);
+    this.instances.push(newRecipeInstance);
+    for (let i = 0; i < this.ingredients.length; i++) {
+      const ingredient = this.ingredients[i];
+      // la quantità dell'ingrediente si ottiene moltiplicando
+      // la quantità totale (che è nota) con la proporzione
+      // del singolo elemento
+      const newQuantity = totIngredientsKnown * ingredient.proportion;
+      newRecipeInstance.addIngredient({ quantity: newQuantity }, ingredient);
+    }
+    return newRecipeInstance;
+  }
+
   _calcProportions() {
     // to compute the proportions, the first recipe instance
     // is taken
@@ -378,6 +182,7 @@ class Recipe {
 
 class RecipeInstance {
   constructor(ingredientsList, recipe) {
+    this.recipe = recipe
     this.ingredients = [];
     this.totIngredients = 0;
     this.addIngredients(ingredientsList, recipe);
@@ -498,7 +303,8 @@ const recipeFatimasBreadFromXWheat = recipeFatimasBread.calcFromIngredient({
 });
 
 // recipe instance
-// const recipeFatimasBreadFromXTot = recipeFatimasBread.calcFromTot(1500);
+const recipeFatimasBreadFromXTot = recipeFatimasBread.calcFromTot(1000);
 
-// console.log(recipeFatimasBread);
-// console.log(recipeFatimasBreadFromXWheat)
+console.log(recipeFatimasBread);
+console.log(recipeFatimasBreadFromXWheat)
+console.log(recipeFatimasBreadFromXTot)
