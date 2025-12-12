6b96b818dce8cbace60804ba2306a21a21f8a14e
Giuseppe Tavella <giuseppetavella8@gmail.com>
2025-12-13 00:14:43 +0100

    add event handlers in recipe UI class

--- assets/js/ui-recipe.js
+++ assets/js/ui-recipe.js
@@ -23,10 +23,67 @@
 // output: table
 
 class RecipeUI {
-  constructor({ outputTableRecipeId, outputTableHaveIngredientId, outputTableHaveTotalId }) {
+  constructor({
+    outputTableRecipeId,
+    outputTableHaveIngredientId,
+    outputTableHaveTotalId,
+
+    inputAddIngredientNameId,
+    inputAddIngredientQuantityId,
+    buttonAddIngredientId,
+
+    inputHaveIngredientNameId,
+    inputHaveIngredientQuantityId,
+
+    inputHaveTotalId,
+  }) {
     this.outputTableRecipeId = outputTableRecipeId;
     this.outputTableHaveIngredientId = outputTableHaveIngredientId;
     this.outputTableHaveTotalId = outputTableHaveTotalId;
+
+    this.inputAddIngredientNameId = inputAddIngredientNameId;
+    this.inputAddIngredientQuantityId = inputAddIngredientQuantityId;
+    this.buttonAddIngredientId = buttonAddIngredientId;
+
+    this.inputHaveIngredientNameId = inputHaveIngredientNameId;
+    this.inputHaveIngredientQuantityId = inputHaveIngredientQuantityId;
+
+    this.inputHaveTotalId = inputHaveTotalId;
+
+    // add the event handlers
+    const pageLoaded = document.readyState === "complete";
+    // if page was loaded, run now
+    if (pageLoaded) {
+      this.addEventHandlers();
+    }
+    // if page is not loaded, run when page is loaded
+    else {
+      window.addEventListener("load", () => {
+        this.addEventHandlers();
+      });
+    }
+  }
+
+  addEventHandlers() {
+    // when user clicks add ingredient
+    document.getElementById(this.buttonAddIngredientId).addEventListener("click", this.handleClickAddIngredient);
+    // when user types in "have ingredient" inputs
+    document.getElementById(this.inputHaveIngredientNameId).addEventListener("keyup", this.handleTypingHaveIngredient);
+    document.getElementById(this.inputHaveIngredientQuantityId).addEventListener("keyup", this.handleTypingHaveIngredient);
+    // when user types in "have total" input
+    document.getElementById(this.inputHaveTotalId).addEventListener("keyup", this.handleTypingHaveTotal);
+  }
+
+  handleClickAddIngredient(ev) {
+    
+  }
+
+  handleTypingHaveIngredient(ev) {
+    // console.log(ev);
+  }
+
+  handleTypingHaveTotal(ev) {
+
   }
 
   refreshOutputTableRecipe(ingredientsList) {
@@ -86,8 +143,17 @@ const recipeUI = new RecipeUI({
   outputTableRecipeId: "table-ricetta",
   outputTableHaveIngredientId: "table-da-ingrediente",
   outputTableHaveTotalId: "table-da-tot-impasto",
+
+  inputAddIngredientNameId: "input-add-ingredient-name",
+  inputAddIngredientQuantityId: "input-add-ingredient-quantity",
+  buttonAddIngredientId: "button-add-ingredient",
+
+  inputHaveIngredientNameId: "input-have-ingredient-name",
+  inputHaveIngredientQuantityId: "input-have-ingredient-quantity",
+
+  inputHaveTotalId: "input-have-total",
 });
 
-console.log(recipeUI)
+console.log(recipeUI);
 
 // recipeUI.
